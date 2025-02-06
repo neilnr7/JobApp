efbde318f65092b17a6d767fcfde635e8ce43c01
Neil <neilrego3@gmail.com>
2025-02-06 13:29:11 +0530

    jsp final

--- src/main/java/com/neil/JobApp/service/JobService.java
+++ src/main/java/com/neil/JobApp/service/JobService.java
@@ -1,4 +1,41 @@
 package com.neil.JobApp.service;
 
+import com.neil.JobApp.model.JobPost;
+import com.neil.JobApp.repo.JobRepo;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
 public class JobService {
+    @Autowired
+    public JobRepo repo;
+
+
+
+    //method to return all JobPosts
+    public List<JobPost> returnAllJobPosts() {
+        return repo.returnAllJobPosts();
+
+
+    }
+
+
+
+
+
+    // ***************************************************************************
+
+
+
+
+
+    // method to add a jobPost
+    public void addJobPost(JobPost jobPost) {
+        repo.addJobPost(jobPost);
+
+    }
+
+
 }
